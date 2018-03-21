0f87eca7652885db23ef0405563f4905bcf23292
sathiyaakdes <37295868+sathiyaakdes@users.noreply.github.com>
2018-03-21 17:56:57 +0530

    second

---
+++ seconds.py
@@ -0,0 +1,5 @@
+seconds = input()
+minutes = seconds%3600/60
+print "With", seconds,"second(s) there are", minutes," minute(s)"
+hours = seconds/3600
+print "With", seconds,"second(s) there are", hours,"hour(s)"
