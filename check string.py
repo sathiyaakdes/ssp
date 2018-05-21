6b19a7b2979ff1e9cf50bc4a3d05eb34a036b0e6
sathiyaakdes <37295868+sathiyaakdes@users.noreply.github.com>
2018-05-21 11:34:58 +0530

    Create check string.py

---
+++ check string.py
@@ -0,0 +1,11 @@
+string=input("Enter string:")
+chara=0
+words=1
+for i in string:
+      chara=chara+1
+      if(i==' '):
+            words=words+1
+print("Number of words in the string:")
+print(words)
+print("Number of characters in the string:")
+print(chara)
