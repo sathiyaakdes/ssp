080fee8bcde300bbca90c2cd5d0f1f9bc3a5775e
sathiyaakdes <37295868+sathiyaakdes@users.noreply.github.com>
2018-05-24 14:27:25 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,10 @@
+sample = ("Python 3.2 is very easy")   
+numeric = 0 
+for i in sample:  
+    if i.isdigit():      
+        numeric +=1      
+   
+    else:    
+        pass  
+  
+print(numeric)  
